81c111a6f22777e01ff6390eb2b918fbe322d37d
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-12-17 16:46:34 -0300

    :bulb: Add solucao.py

---
+++ PROBLEMA_1/solucao.py
@@ -0,0 +1,42 @@
+def find_values(dictionary_list):
+    """
+    Essa função recebe uma lista de dicionarios e retorna a lista dos valores
+    associados à chave 'nome' (caso exista), sem que haja repetição.
+    """
+
+    values_list = []  # Inicializa a lista de resultados (vazia)
+
+    for dictionary in dictionary_list:  # Laço que acessa cada dicionário na lista de dicionários
+        if "nome" in dictionary:  # Caso exista a chave 'nome', prossiga
+            # Caso o valor da chave 'nome' ainda não exista na lista de
+            # resultados:
+            if dictionary["nome"] not in values_list:
+                # Insira na lista de resultados
+                values_list.append(dictionary["nome"])
+            else:
+                print("Ops! O valor '{}' é uma duplicata!"  # Caso já tenha sido inserido, imprima no console:
+                      .format(dictionary["nome"]))
+                print("Portanto, não foi armazenado.")
+        else:
+            # Caso não exista a chave 'nome', imprima no console:
+            print("Não foi encontrado o índice 'nome' para esse dicionário.")
+
+    return values_list
+
+
+def __main__():
+    # Declara lista de dicionários
+    dictionary_list = [
+        {"nome": "José"},
+        {"nome": "Pedro"},
+        {"nome": "Robson"},
+        {"nome": "Mateus"},
+        {"nome": "Iago"},
+        {"nome": "José"}
+    ]
+
+    # Chama a função e imprime seu valor de retorno (lista) como string.
+    print("O resultado final é: " + str(find_values(dictionary_list)))
+
+
+__main__()
