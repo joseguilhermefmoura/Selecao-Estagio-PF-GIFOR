b795276f06bb0eb83795c815560b1ed793b46a94
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-12-18 18:59:59 -0300

    :arrow_up: Changed quotes

--- PROBLEMA_1/solucao.py
+++ PROBLEMA_1/solucao.py
@@ -1,25 +1,25 @@
 def find_values(dictionary_list):
-    """
+    '''
     Essa função recebe uma lista de dicionarios e retorna a lista dos valores
     associados à chave 'nome' (caso exista), sem que haja repetição.
-    """
+    '''
 
     values_list = []  # Inicializa a lista de resultados (vazia)
 
     for dictionary in dictionary_list:  # Laço que acessa cada dicionário na lista de dicionários
-        if "nome" in dictionary:  # Caso exista a chave 'nome', prossiga
-            if dictionary["nome"] not in values_list:
+        if 'nome' in dictionary:  # Caso exista a chave 'nome', prossiga
+            if dictionary['nome'] not in values_list:
                 # Caso o valor da chave 'nome' ainda não exista na lista de resultados:
                 # Insira na lista de resultados
-                values_list.append(dictionary["nome"])
+                values_list.append(dictionary['nome'])
             else:
                 # Caso já tenha sido inserido, imprima no console:
-                print("Ops! O valor '{}' é uma duplicata!"
-                      .format(dictionary["nome"]))
-                print("Portanto, não foi armazenado.")
+                print('Ops! O valor "{}" é uma duplicata!'
+                      .format(dictionary['nome']))
+                print('Portanto, não foi armazenado.')
         else:
             # Caso não exista a chave 'nome', imprima no console:
-            print("Não foi encontrado o índice 'nome' para esse dicionário.")
+            print('Não foi encontrado o índice "nome" para esse dicionário.')
 
     return values_list
 
@@ -27,16 +27,16 @@ def find_values(dictionary_list):
 def __main__():
     # Declara lista de dicionários
     dictionary_list = [
-        {"nome": "José"},
-        {"nome": "Pedro"},
-        {"nome": "Robson"},
-        {"nome": "Mateus"},
-        {"nome": "Iago"},
-        {"nome": "José"}
+        {'nome': 'José'},
+        {'nome': 'Pedro'},
+        {'nome': 'Robson'},
+        {'nome': 'Mateus'},
+        {'nome': 'Iago'},
+        {'nome': 'José'}
     ]
 
     # Chama a função e imprime seu valor de retorno (lista) como string.
-    print("O resultado final é: " + str(find_values(dictionary_list)))
+    print('O resultado final é: ' + str(find_values(dictionary_list)))
 
 
 __main__()
--- PROBLEMA_1/solucao_elaborada.py
+++ PROBLEMA_1/solucao_elaborada.py
@@ -1,32 +1,34 @@
 def find_values(dictionary_list):
-    """
+    '''
     Essa função recebe uma lista de dicionarios e retorna a lista dos valores
     associados à chave 'nome' (caso exista), sem que haja repetição.
-    """
+    '''
 
     # Solução em uma linha. Explicação:
 
-    """
+    '''
     Um set não pode possuir duplicatas, então transformamos o nosso return em um set,
-    que está englobando um compressor de lista ("[]", responsável por transformar em lista),
+    que está englobando um compressor de lista ('[]', responsável por transformar em lista),
     que engloba um laço de uma linha, que itera cada dicionario na lista de dicionarios e retorna
     o valor associado a chave 'nome' - caso este exista.
-    """
-    return set([dictionary['nome'] for dictionary in dictionary_list if 'nome' in dictionary])
+    '''
+    return set([dictionary['nome']
+                for dictionary in dictionary_list if 'nome' in dictionary])
+
 
 def __main__():
     # Declara lista de dicionários
     dictionary_list = [
-        {"nome": "José"},
-        {"nome": "Pedro"},
-        {"nome": "Robson"},
-        {"nome": "Mateus"},
-        {"nome": "Iago"},
-        {"nome": "José"}
+        {'nome': 'José'},
+        {'nome': 'Pedro'},
+        {'nome': 'Robson'},
+        {'nome': 'Mateus'},
+        {'nome': 'Iago'},
+        {'nome': 'José'}
     ]
 
     # Chama a função e imprime seu valor de retorno (lista) como string.
-    print("O resultado final é: " + str(find_values(dictionary_list)))
+    print('O resultado final é: ' + str(find_values(dictionary_list)))
 
 
 __main__()
